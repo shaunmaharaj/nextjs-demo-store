504192a0fff99a7211e3e69d623ffed8191e778e
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2021-02-23 09:45:51 -0500

    - remove static dir

--- pages/product.js
+++ pages/product.js
@@ -26,7 +26,7 @@ ProductPage.getInitialProps = async ({ query: { id } }) => {
       ...data,
       image: imageId
         ? main_images.find(img => img.id === imageId).link.href
-        : '/static/moltin-light-hex.svg'
+        : '/moltin-light-hex.svg'
     }
   }
 }
